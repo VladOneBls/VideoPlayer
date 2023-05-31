0ba6e22c40700ac68f9c79e10dff24dcb761edbf
Vlad Balas <balash_vlad@yahoo.com>
2023-05-31 20:48:16 +0100

    Progress Bar (Seek)

--- script.js
+++ script.js
@@ -46,7 +46,12 @@ function updateProgress() {
     duration.textContent = `${displayTime(video.duration)}`;
 }
 
-
+// Click to seek within the video
+function setProgress(e) {
+    const newTime = e.offsetX / progressRange.offsetWidth; // 'where we clicked' divided by 'total width of the progress range'
+    progressBar.style.width = `${newTime * 100}%`;
+    video.currentTime = newTime * video.duration;
+}
 
 // *** VOLUME CONTROLS *** //
 
@@ -64,3 +69,4 @@ playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
